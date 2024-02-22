f5d590069b39a3a07251707ba26427260a840cd8
ValberF <valberfrsantos@gmail.com>
2024-02-22 14:18:40 -0300

    First commit

---
+++ ExemplosAula/Modulo5/MvcMovie/Models/User.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Password { get; set; }
+}
