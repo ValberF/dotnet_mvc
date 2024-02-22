c09b532322937386d490829def11e171d494e4b9
ValberF <valberfrsantos@gmail.com>
2024-02-22 16:33:20 -0300

    MVC user

--- ExemplosAula/Modulo5/MvcMovie/Models/User.cs
+++ ExemplosAula/Modulo5/MvcMovie/Models/User.cs
@@ -5,6 +5,7 @@ namespace MvcMovie.Models;
 public class User
 {
     public int Id { get; set; }
-    public string? Name { get; set; }
-    public string? Password { get; set; }
+    public required string Name { get; set; }
+    public required string Email { get; set; }
+    public required string Password { get; set; }
 }
