fc0bc02eb6cb783818f93d98ad6c0d0d26c22b51
kayquepiton <kayquepiton@gmail.com>
2024-02-28 14:19:03 -0300

    login

--- ExemplosAula/Modulo5/MvcMovie/Controllers/LoginController.cs
+++ ExemplosAula/Modulo5/MvcMovie/Controllers/LoginController.cs
@@ -1,39 +1,88 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MvcMovie.Data;
-using MvcMovie.Data.Security;
 using MvcMovie.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Security.Cryptography;
 
-namespace MvcMovie.Controllers;
-public class LoginController : Controller
+namespace MvcMovie.Controllers
 {
-   private readonly MvcMovieContext _context;
-
-    public LoginController(MvcMovieContext context)
+    public class LoginController : Controller
     {
-        _context = context;
+        private readonly MvcMovieContext _context;
+        private readonly IConfiguration _configuration;
+
+        public LoginController(MvcMovieContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        // GET: Login/Index
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: Login/Authenticate
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login([Bind("Email,Password")] Login user)
+        {
+            if (ModelState.IsValid)
+            {
+                user.Password = HashPassword(user.Password ?? "");
+                var userInDb = await _context.User.FirstOrDefaultAsync(u => u.Email == user.Email && u.Password == user.Password);
+
+               if (userInDb != null)
+               {
+                  // Se o usuário for autenticado com sucesso, gere um token JWT
+                  var token = GenerateJwtToken(userInDb);
+                  HttpContext.Session.SetString("JwtToken", token);
+                  return Ok(new { token });
+               }
+           
+            }
+            return Unauthorized();
+        }
+
+        // Método para gerar um token JWT
+        private string GenerateJwtToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration["Jwt:SecretKey"]);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                    new Claim(ClaimTypes.Email, user.Email)
+                }),
+                Expires = DateTime.UtcNow.AddHours(Convert.ToDouble(_configuration["Jwt:ExpiryInHours"])),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
+        public static string HashPassword(string password)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
     }
-
-    // GET: Login/Index
-    public IActionResult Index()
-   {
-      return View();
-   }
-
-   // POST: Login/Authenticate
-   // To protect from overposting attacks, enable the specific properties you want to bind to.
-   // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-   [HttpPost]
-   [ValidateAntiForgeryToken]
-   public async Task<IActionResult> Login([Bind("Email,Password")] Login user)
-   {
-      if (ModelState.IsValid)
-      {
-         user.Password = Utils.HashPassword(user.Password ?? "");
-         var userInDb = await _context.User.FirstOrDefaultAsync(u => u.Email == user.Email && u.Password == user.Password);
-         //if (userInDb.Email == user.Email)
-         return RedirectToAction("Index", "Home");
-      }
-      return View(user);
-   }
 }
